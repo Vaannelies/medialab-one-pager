11da10d31254213945ee2e5a7667827f10238273
Annelies <0962949@hr.nl>
2021-02-23 18:36:34 +0100

    commit dist folder

---
+++ dist/precache-manifest.7e1ca2ff3a641487cdaa92f92e6714ca.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6061807482fd8a4b9fe8",
+    "url": "/medialab-one-pager/css/app.654a66dd.css"
+  },
+  {
+    "revision": "f31bb403285d8a8d6e7f7e893bdbad87",
+    "url": "/medialab-one-pager/img/beach_flush.f31bb403.jpg"
+  },
+  {
+    "revision": "56c459598bb59195ce286f39d1efea5c",
+    "url": "/medialab-one-pager/img/hero_image.56c45959.jpg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/medialab-one-pager/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "d25183f1bc432043958b0648acc0158c",
+    "url": "/medialab-one-pager/index.html"
+  },
+  {
+    "revision": "5644c08f19bb5e3c7623",
+    "url": "/medialab-one-pager/js/about.e5e8626a.js"
+  },
+  {
+    "revision": "6061807482fd8a4b9fe8",
+    "url": "/medialab-one-pager/js/app.15d4172f.js"
+  },
+  {
+    "revision": "dcdd5a6604ac31f873db",
+    "url": "/medialab-one-pager/js/chunk-vendors.84776926.js"
+  },
+  {
+    "revision": "b92c94dff3ede5176dcc74322fb64b12",
+    "url": "/medialab-one-pager/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/medialab-one-pager/robots.txt"
+  }
+]);
