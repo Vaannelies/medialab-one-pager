1888a6415d0ef9d518302b3cec03b1a19e480523
Annelies <0962949@hr.nl>
2021-02-24 11:17:44 +0100

    ConceptBlock: triangle divider

--- src/components/ConceptBlock.vue
+++ src/components/ConceptBlock.vue
@@ -5,16 +5,57 @@
     </div>
 </template>
 
+<script lang="ts">
+import { Vue, Options } from 'vue-class-component'
+import StickyNotes from 'StickyNotes.vue'
+
+@Options({
+    components: {
+        StickyNotes
+    }
+})
+export default class ConceptBlock extends Vue {
+
+}
+</script>
+
 <style scoped>
+    /* .fade {
+        z-index: -2; 
+        height: 200px;
+        margin-top: -100px; 
+            background: linear-gradient(
+            180deg,
+            #3a1f1300 0%,
+             #ffffff 300px, 
+            #ffffff 50%
+        );
+        rotate: 180deg;
+    } */
     .wide__container {
         width: 100%;
         height: 100vh;
-        /* background-image: url('https://images.unsplash.com/photo-1496805713444-03e7b6eca934?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1268&q=80'); */
         text-align: center;
         background-color: white;
-    
-     
     }
+
+    /* .layer {
+        z-index: -2;
+   background-image: url('https://images.unsplash.com/photo-1496805713444-03e7b6eca934?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1268&q=80');
+    rotate: 180deg; 
+    background-position-y: 100%;
+    background-size: cover;
+    margin-top: 0;
+    height: 200px;
+    transform: rotate(180deg);
+     background: linear-gradient(
+            180deg,
+            #3a1f13e5 0%,
+             #ffffff00 100px, 
+            #e23333db 100%
+        ),
+         url('https://images.unsplash.com/photo-1496805713444-03e7b6eca934?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1268&q=80'),
+    } */
 
     .title {
         font-family: "Bebas Neue";
